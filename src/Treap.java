6d8f034b31b191684006defc866b6c2122be4749
ryumix22 <dimac2278@yandex.ru>
2019-01-28 20:37:18 +0300

    treap

--- src/Treap.java
+++ src/Treap.java
@@ -1,64 +1,90 @@
-import javafx.util.Pair;
-import org.jetbrains.annotations.NotNull;
 
+import java.util.Comparator;
+import java.util.Random;
 import java.util.Set;
 
-public class Treap<T> implements Comparable<Object> {
+public class Treap<T> implements Comparator<Object> {
     private T x;
     private int y;
 
-    private Treap Left;
-    private Treap Right;
+    private Treap<T> Left;
+    private Treap<T> Right;
 
-    public Treap(T x, int y, Treap left, Treap right) {
+    public Treap(T x, int y, Treap<T> left, Treap<T> right) {
         this.x = x;
         this.y = y;
         Left = left;
         Right = right;
     }
 
-    public Treap merge(Treap<T> l, Treap<T> r){
-        if (l == null) return r;
-        if (r == null) return l;
 
-        if (l.y > r.y)
+    public Treap<T> merge(Treap<T> treap1, Treap<T> treap2){
+        if (treap1 == null) return treap2;
+        if (treap2 == null) return treap1;
+        else
+        if (treap1.y > treap2.y)
         {
-            Treap newR = merge(l.Right, r);
-            return new Treap(l.x, l.y, l.Left, newR);
+            treap1.Right = merge(treap1.Right, treap2);
+            return treap1;
         } else {
-            Treap newL = merge(l, r.Left);
-            return new Treap(r.x, r.y, newL, r.Right);
+            treap2.Left = merge(treap1, treap2.Left);
+            return treap2;
         }
     }
 
-    public Pair<Treap, Treap> split(T key, Treap l, Treap r){
-        Treap newTree = null;
-        if (this.compareTo(key) <= 0) {
-            if (Right == null) r = null;
-            else Right.split(key, newTree, r);
-            //l = new Treap(this.x, y, Left, newTree);
-        } else {
-            if (Left == null) l = null;
-            else Left.split(key, l, newTree);
-            //r = new Treap(this.x, y, newTree, Right);
-        }
-        return null;
-    }
+    private Treap<T>[] pairTreap = new Treap[2];
 
-    /*
-    public Treap add(T x) {
-        Treap left = null;
-        Treap right = null;
-        split(x, left, right);
+    public Treap<T>[] split(T key, Treap<T> treap) {
+        if (treap == null) {
+            pairTreap[0] = null;
+            pairTreap[1] = null;
+            return pairTreap;
+        } else if (compare(key, treap.x) > 0) {
+            pairTreap = split(key, treap.Right);
+            treap.Right = pairTreap[0];
+            pairTreap[0] = treap;
+            return pairTreap;
+        } else {
+            pairTreap = split(key, treap.Left);
+            treap.Left = pairTreap[1];
+            pairTreap[1] = treap;
+            return pairTreap;
+        }
     }
-    */
     public void build(Set<T> elements) {
 
     }
 
+    public void add(T vertex, Treap<T> treap) {
+        Random random = new Random();
+        Treap<T>[] pair = split(vertex, treap);
+        Treap<T> oneVertex = new Treap<>(vertex, random.nextInt(100), null, null);
+        Treap<T> tr = merge(merge(pair[0], oneVertex), pair[1]);
+        this.x = tr.x;
+        this.y = tr.y;
+        this.Left = tr.Left;
+        this.Right = tr.Right;
+
+    }
+
+    public void remove(T vertex, Treap<T> treap) {
+
+    }
+
+    public static void main(String[] args) {
+        Integer int1 = 5;
+        Integer int2 = 4;
+        System.out.println(int1.compareTo(int2));
+    }
+
+
+    //@Override
+    //public int compareTo(@NotNull Object o) {
+      //  return 0;
+    //}
 
     @Override
-    public int compareTo(@NotNull Object o) {
+    public int compare(Object o1, Object o2) {
         return 0;
     }
 }
